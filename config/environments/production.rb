8ad6eabecf1bc8ac0c0b5c0dbb2983df77db81f3
vlad <student3.oxygen.it.school@gmail.com>
2018-02-19 11:53:37 +0200

    fix email delivery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,11 +61,21 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "project_manager_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = {host: 'https://nameless-tor-10914.herokuapp.com/' }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options = { host => 'nameless-tor-10914.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  Authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["auto1audio@gmail.com"],
+  password: ENV["vlad3786117"]
+  }
+config.action_mailer.perform_deliveries = true
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
@@ -89,3 +99,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+
+
