17184cd1ed1adecf7109c1d53d455d3527a1c36f
vlad <student3.oxygen.it.school@gmail.com>
2018-02-19 12:29:20 +0200

    fix email delivery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
-  before_action :check_blank_profile
+  #before_action :check_blank_profile
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
